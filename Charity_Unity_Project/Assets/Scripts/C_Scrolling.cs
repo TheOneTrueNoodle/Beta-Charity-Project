4f045e300cbd6324d9c5277b925db2809ac355af
SmolConnor <77941492+SmolConnor@users.noreply.github.com>
2023-04-27 16:38:16 +0100

    scrolling can now be paused with H
    scrolling can be paused with H no UI buttons at the moment. made a seperate script for the space background so it continues moving.

--- Charity_Unity_Project/Assets/Scripts/C_Scrolling.cs
+++ Charity_Unity_Project/Assets/Scripts/C_Scrolling.cs
@@ -9,8 +9,26 @@ public class C_Scrolling : MonoBehaviour
     private RawImage img;
     [SerializeField]
     private float x, y;
+    private bool scrollPaused = false;
     void Update()
     {
-        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (scrollPaused == false)
+            {
+                scrollPaused = true;
+            }
+            else if (scrollPaused == true)
+            {
+                scrollPaused = false;
+            }
+        }
+        if (scrollPaused == false)
+        {
+
+            img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+        }
     }
+
+
 }

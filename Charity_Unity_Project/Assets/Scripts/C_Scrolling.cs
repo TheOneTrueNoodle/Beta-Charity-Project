b888e7639359376c73bfee9b92b81876cb7b52fb
SmolConnor <77941492+SmolConnor@users.noreply.github.com>
2023-05-03 19:12:34 +0100

    Working pause Button
    New working pause button

--- Charity_Unity_Project/Assets/Scripts/C_Scrolling.cs
+++ Charity_Unity_Project/Assets/Scripts/C_Scrolling.cs
@@ -8,6 +8,10 @@ public class C_Scrolling : MonoBehaviour
     [SerializeField]
     private RawImage img;
     [SerializeField]
+    private GameObject pauseButton;
+    [SerializeField]
+    private GameObject playButton;
+    [SerializeField]
     private float x, y;
     private bool scrollPaused = false;
     void Update()
@@ -17,16 +21,35 @@ public class C_Scrolling : MonoBehaviour
             if (scrollPaused == false)
             {
                 scrollPaused = true;
+                pauseButton.SetActive(false);
+                playButton.SetActive(true);
             }
             else if (scrollPaused == true)
             {
                 scrollPaused = false;
+                pauseButton.SetActive(true);
+                playButton.SetActive(false);
             }
         }
         if (scrollPaused == false)
         {
 
             img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+        }
+    }
+    public void PauseScrolling()
+    {
+        if (scrollPaused == false)
+        {
+            scrollPaused = true;
+            pauseButton.SetActive(false);
+            playButton.SetActive(true);
+        }
+        else if (scrollPaused == true)
+        {
+            scrollPaused = false;
+            pauseButton.SetActive(true);
+            playButton.SetActive(false);
         }
     }
 
